3794c4f89dd334fc45458b4401422938397d7bb7
Betina <39097891+bhristova@users.noreply.github.com>
2024-01-18 22:16:49 +0200

    remove complex numbers instead of prime numbers

--- prolog/2024-01-06-1ex.pl
+++ prolog/2024-01-06-1ex.pl
@@ -1,8 +1,8 @@
-// задача 1 от контролно на 06.01.2024
-// Тегло на списък от естествени числа наричаме сумата на всички елементи от списъка.
-// Да се дефинира на пролог предикат p(X,Y), който по даден списък X от списъци от естествени числа
-// генерира в Y списък без повторения от точно тези естествени числа,
-// които са тегла на списък X, съдържащ само прости числа. 
+/* задача 1 от контролно на 06.01.2024
+Тегло на списък от естествени числа наричаме сумата на всички елементи от списъка.
+Да се дефинира на пролог предикат p(X,Y), който по даден списък X от списъци от естествени числа
+генерира в Y списък без повторения от точно тези естествени числа,
+които са тегла на списък X, съдържащ само прости числа. */
 
 append([], B, B).
 append([H|T],B,[H|T1]):-append(T,B,T1).
@@ -25,7 +25,7 @@ generate_between(S,E,R):-S<E,S1 is S+1,generate_between(S1,E,R).
 is_prime(X):-not((generate_between(2,X-1,R),0 is mod(X,R))),X\=1.
 
 remove_composite_numbers([],[]).
-remove_composite_numbers([H|T],[H|R]):-not(is_prime(H)), remove_composite_numbers(T,R).
-remove_composite_numbers([H|T],R):-is_prime(H), remove_composite_numbers(T,R).
+remove_composite_numbers([H|T],[H|R]):-is_prime(H), remove_composite_numbers(T,R).
+remove_composite_numbers([H|T],R):-not(is_prime(H)), remove_composite_numbers(T,R).
 
 p(X,Y):-get_lists_weights(X,Y1),remove_duplicates(Y1,Y2),remove_composite_numbers(Y2,Y).
